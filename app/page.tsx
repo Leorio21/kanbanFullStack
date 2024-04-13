4d54259e46dbeb1e7e7ce7768a1810f963079e6a
Leorio21 <Ptitlue@gmail.com>
2024-04-13 18:44:45 +0200

    Fix: localStorage error

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,20 @@
 "use client";
+import { useEffect, useState } from "react";
 import Board from "./Components/Board/Board";
 import { loadData, useBoardsStore } from "./Stores/useBoards";
 
 export default function Home() {
+  const [data, setData] = useState(false);
   const dataLoaded = useBoardsStore((state) => state.dataLoaded);
-  if (!dataLoaded && !localStorage.getItem("boards")) {
-    loadData();
-  }
-  if (dataLoaded) {
+
+  useEffect(() => {
+    if (!dataLoaded && !localStorage.getItem("boards")) {
+      loadData();
+    }
+    setData(true);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  if (data) {
     return (
       <>
         <Board />
